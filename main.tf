f20d70becea310bed46e03255c76f88a378cfb5c
Demoh-77 <x23315679@student.ncirl.ie>
2025-08-22 13:58:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,29 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
-  location                 = "US"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project     = "famous-rhythm-469720-e4"  # Your GCP project ID
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
+}
+
+resource "google_storage_bucket" "my-bucket3" {
+  name                     = "tt-githubdemo-bucket-003-famous-rhythm-469720-e4"
+  project                  = "famous-rhythm-469720-e4"
+  location                 = "EU"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
-#
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
-  location                 = "US"
+
+resource "google_storage_bucket" "my-bucket4" {
+  name                     = "tt-githubdemo-bucket-004-famous-rhythm-469720-e4"
+  project                  = "famous-rhythm-469720-e4"
+  location                 = "EU"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
-#
-### dummy
