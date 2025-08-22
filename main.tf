de991f71fe8b5a8735c6fac1abdbb883251d1615
Demoh-77 <x23315679@student.ncirl.ie>
2025-08-22 15:23:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,22 @@ provider "google" {
   zone        = "europe-west1-b"
 }
 
+resource "google_storage_bucket" "my-bucket1" {
+  name                     = "tt-githubdemo-bucket-001-famous-rhythm-469720-e4"
+  project                  = "famous-rhythm-469720-e4"
+  location                 = "EU"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "tt-githubdemo-bucket-002-famous-rhythm-469720-e4"
+  project                  = "famous-rhythm-469720-e4"
+  location                 = "EU"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
 resource "google_storage_bucket" "my-bucket3" {
   name                     = "tt-githubdemo-bucket-003-famous-rhythm-469720-e4"
   project                  = "famous-rhythm-469720-e4"
